089ada520a81c52b1260762228cdd7a8b9caa470
thislis <ckwone12@naver.com>
2024-08-05 01:42:32 +0900

    start

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Menu(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_type = Column(String, index=True)
+    question = Column(String, index=True)
+    answer = Column(String, index=True)
