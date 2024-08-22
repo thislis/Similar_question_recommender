af23e7db0d2319f85d60b586423ff97331d9588a
rzbsys <rzbsys@unist.ac.kr>
2024-08-23 02:53:31 +0900

    데이터베이스 모델의 이름 및 Base의 위치를 조정했어요

--- src/database/models.py
+++ src/database/models.py
@@ -1,10 +1,18 @@
 from sqlalchemy import Column, Integer, String
-from src.database.database import Base
+from sqlalchemy.ext.declarative import declarative_base
 
-class Menu(Base):
+from src.database.database import Base
+from src.database.database import engine
+
+Base = declarative_base()
+
+class Question(Base):
     __tablename__ = "question"
 
     id = Column(Integer, primary_key=True, index=True)
     question_type = Column(String, index=True)
     question = Column(String, index=True)
     answer = Column(String, index=True)
+
+
+Base.metadata.create_all(bind=engine)
