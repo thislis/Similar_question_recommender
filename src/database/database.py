5c3ab7ece1b831c33a16864988cd7eb09c62cdc7
rzbsys <rzbsys@unist.ac.kr>
2024-08-23 02:52:12 +0900

    database의 get_db의 위치를 조금 바꿨어요.

--- src/database/database.py
+++ src/database/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///./Similar_question_recommender.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
